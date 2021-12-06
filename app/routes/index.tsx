354c29d7ed17ed89a9efbf08dcb84cebe4428835
Ian Banda <ian.d.banda@gmail.com>
2021-12-06 23:17:03 +0200

    add layout

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,4 +1,14 @@
-import type { MetaFunction } from 'remix';
+import { Form, MetaFunction, useLoaderData } from 'remix';
+import type { LoaderFunction } from 'remix';
+import { client } from '~/api/client';
+import BestPodcasts from '~/components/BestPodcasts';
+import { FetchBestPodcastsPayload } from 'podcast-api';
+
+export const loader: LoaderFunction = async () => {
+  const response = await client.fetchBestPodcasts();
+  const { data } = response;
+  return data;
+};
 
 // https://remix.run/api/conventions#meta
 export const meta: MetaFunction = () => {
@@ -10,5 +20,43 @@ export const meta: MetaFunction = () => {
 
 // https://remix.run/guides/routing#index-routes
 export default function Index() {
-  return <div>Hello Remix</div>;
+  const data = useLoaderData<FetchBestPodcastsPayload>();
+  return (
+    <>
+      <div className="grid md:grid-cols-2 py-8">
+        <div className="col-span-1 flex items-center text-white">
+          <div>
+            <h1 className="text-6xl  font-bold tracking-tighter">
+              Best podcasts for curious minds
+            </h1>
+            <h2 className="mt-8">
+              Our pick of the best podcasts on Spotify, Apple Podcasts
+              and more covering technology, culture, science, politics
+              and new ideas
+            </h2>
+            <Form>
+              <div className="">
+                <label>
+                  <input
+                    className="mt-4 text-black p-4  rounded-l "
+                    type="text"
+                  />
+                </label>
+                <button className="p-4 bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500 rounded-r">
+                  Search
+                </button>
+              </div>
+            </Form>
+          </div>
+        </div>
+        <div className="col-span-1 flex justify-center overflow-hidden ">
+          <img
+            className=" xl:max-w-xl transform rotate-45 md:-mr-28 xl:-mr-44"
+            src="/mic-front-premium.png"
+          />
+        </div>
+      </div>
+      <BestPodcasts podcasts={data.podcasts} />
+    </>
+  );
 }
