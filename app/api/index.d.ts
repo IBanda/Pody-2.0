e38f921c99cfaf4e3ccf1f4c53f8c98a9abe4009
Ian Banda <ian.d.banda@gmail.com>
2021-12-10 10:19:22 +0200

    add fetchPodcastById

--- app/api/index.d.ts
+++ app/api/index.d.ts
@@ -24,10 +24,38 @@ declare module 'podcast-api' {
     podcasts: BestPodcast[];
   };
 
+  type FetchPodcastByIdParams = {
+    id: string;
+  };
+
+  type Episode = {
+    id: string;
+    link: string;
+    audio: string;
+    image: string;
+    title: string;
+    thumbnail: string;
+    description: string;
+    audio_length_sec: number;
+  };
+
+  type FetchPodcastByIdPayload = {
+    id: string;
+    image: string;
+    description: string;
+    thumbnail: string;
+    title: string;
+    publisher: string;
+    episodes: Episode[];
+  };
+
   type ClientInstance = {
     fetchBestPodcasts: (
       params?: FetchBestPodcastsParams
     ) => Promise<{ data: FetchBestPodcastsPayload }>;
+    fetchPodcastById: (
+      params: FetchPodcastByIdParams
+    ) => Promise<{ data: FetchPodcastByIdPayload }>;
   };
 
   type client = (config: Config) => ClientInstance;
