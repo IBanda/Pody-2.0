5b2c084552d986436087ebc05e01648ecc744824
Ian Banda <ian.d.banda@gmail.com>
2021-12-14 00:40:32 +0200

    use apiKey

--- app/api/client.server.ts
+++ app/api/client.server.ts
@@ -1,6 +1,7 @@
 import { Client } from 'podcast-api';
 
+//passing null here allows us to use the test data from the service
 export const client = Client({
-  // apiKey: String(process.env.API_KEY),
-  apiKey: null,
+  apiKey: String(process.env.API_KEY),
+  // apiKey: null,
 });
