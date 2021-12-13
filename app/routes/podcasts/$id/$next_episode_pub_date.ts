cca67031387bf1ef099a796a3e14093a0e132426
Ian Banda <ian.d.banda@gmail.com>
2021-12-13 23:20:08 +0200

    add episodes pagination resource route

---
+++ app/routes/podcasts/$id/$next_episode_pub_date.ts
@@ -0,0 +1,20 @@
+import type { LoaderFunction } from 'remix';
+import invariant from 'tiny-invariant';
+import { client } from '~/api/client.server';
+
+export const loader: LoaderFunction = async ({ params }) => {
+  const { id, next_episode_pub_date } = params;
+
+  invariant(id, 'ID is required');
+  invariant(
+    next_episode_pub_date,
+    'next_episode_pub_date is required'
+  );
+
+  const response = await client.fetchPodcastById({
+    id,
+    next_episode_pub_date: +next_episode_pub_date,
+  });
+  const { data } = response;
+  return data;
+};
