875a5bc1a34befd6169f4e07f868be31d5a20998
Ian Banda <ian.d.banda@gmail.com>
2021-12-14 00:44:05 +0200

    test setup

---
+++ jest.config.ts
@@ -0,0 +1,22 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+export default {
+  clearMocks: true,
+
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
+  },
+
+  setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
+
+  testEnvironment: 'jsdom',
+
+  transform: {
+    '^.+\\.[t|j]sx?$': 'ts-jest',
+  },
+};
