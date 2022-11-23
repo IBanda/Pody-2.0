37f48ca9bdc25797754fe565942447034f17c136
Ian Banda <ian.d.banda@gmail.com>
2022-11-23 09:49:25 +0200

    used apiKey constant

--- app/api/client.server.ts
+++ app/api/client.server.ts
@@ -1,7 +1,11 @@
 import { Client } from 'podcast-api';
 
 //passing null here allows us to use the test data from the service
+const apiKey =
+  process.env.NODE_ENV === 'development'
+    ? null
+    : String(process.env.API_KEY);
+
 export const client = Client({
-  apiKey: String(process.env.API_KEY),
-  // apiKey: null,
+  apiKey,
 });
